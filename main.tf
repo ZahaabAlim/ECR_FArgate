4287bcb79bf84c4b0abf3b37f6025a045052a4ec
ZahaabAlim <Zahaabalim@gmail.com>
2024-10-22 12:48:31 +0530

    scheduler added

--- main.tf
+++ main.tf
@@ -7,38 +7,37 @@ terraform {
     # encrypt        = true
   }
 }
- 
+
 provider "aws" {
   region     = "us-east-1"
- 
 }
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
- 
+
 resource "aws_subnet" "subnet1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
- 
+
 resource "aws_subnet" "subnet2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
 }
- 
+
 resource "aws_security_group" "ecs_sg" {
   vpc_id = aws_vpc.main.id
- 
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -46,12 +45,12 @@ resource "aws_security_group" "ecs_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
- 
+
 # Create ECS Cluster
 resource "aws_ecs_cluster" "fargate_cluster" {
     name = "fargate-cluster"
 }
- 
+
 # Create IAM Role for ECS Task Execution
 resource "aws_iam_role" "ecs_task_execution_role" {
     name = "ecsTaskExecutionRole"
@@ -69,7 +68,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
         "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
   ]
 }
- 
+
 # Create ECS Task Definition
 resource "aws_ecs_task_definition" "fargate_task" {
     family                   = "fargate-task"
@@ -85,13 +84,13 @@ resource "aws_ecs_task_definition" "fargate_task" {
         memory = 512
         essential = true
     }])
- 
+
     runtime_platform {
         operating_system_family = "LINUX"
         cpu_architecture        = "X86_64"
     }
 }
- 
+
 # Create ECS Service
 resource "aws_ecs_service" "fargate_service" {
     name            = "fargate-service"
@@ -102,4 +101,49 @@ resource "aws_ecs_service" "fargate_service" {
     subnets         = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
     security_groups = [aws_security_group.ecs_sg.id]
   }
+}
+
+# Create EventBridge Rule
+resource "aws_cloudwatch_event_rule" "ecs_task_scheduler" {
+  name                = "ecs-task-scheduler"
+  description         = "Schedule ECS task to run periodically"
+  schedule_expression = "rate(1 hour)" # Adjust the schedule as needed
+}
+
+# Create EventBridge Target
+resource "aws_cloudwatch_event_target" "ecs_task_target" {
+  rule      = aws_cloudwatch_event_rule.ecs_task_scheduler.name
+  arn       = aws_ecs_cluster.fargate_cluster.arn
+  role_arn  = aws_iam_role.ecs_task_execution_role.arn
+  ecs_target {
+    task_definition_arn = aws_ecs_task_definition.fargate_task.arn
+    task_count          = 1
+    launch_type         = "FARGATE"
+    network_configuration {
+      subnets         = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+      security_groups = [aws_security_group.ecs_sg.id]
+      assign_public_ip = "ENABLED"
+    }
+  }
+}
+
+# Allow EventBridge to invoke ECS tasks
+resource "aws_iam_role_policy" "allow_eventbridge_to_invoke_ecs" {
+  name   = "AllowEventBridgeToInvokeECS"
+  role   = aws_iam_role.ecs_task_execution_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "ecs:RunTask"
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = "iam:PassRole"
+        Resource = "*"
+      }
+    ]
+  })
 }
